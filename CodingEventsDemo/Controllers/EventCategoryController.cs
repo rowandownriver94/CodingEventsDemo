e8c08d3b34550adfa11c5e69115bd56231c286ea
Rowan Polster <rowandownriver94@gmail.com>
2021-10-13 19:59:45 -0500

    added EventCategory view

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.eventCategories.ToList();
+            return View(categories);
+        }
+    }
+}
